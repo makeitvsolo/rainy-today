83990d823ed434f7bbe0a1b9f6ecb3dd65fad560
makeitvsolo <makeitvsolo@gmail.com>
2023-09-23 00:09:10 +0300

    refactor: rename package from `favourite` to `account`

--- src/test/java/com/makeitvsolo/rainytoday/model/account/AccountTests.java
+++ src/test/java/com/makeitvsolo/rainytoday/model/account/AccountTests.java
@@ -1,4 +1,4 @@
-package com.makeitvsolo.rainytoday.model.favourite;
+package com.makeitvsolo.rainytoday.model.account;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
