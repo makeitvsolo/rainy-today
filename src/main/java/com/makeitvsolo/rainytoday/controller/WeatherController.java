9e0a44909be8a24aec4e6aec0cc55a014adfe784
makeitvsolo <makeitvsolo@gmail.com>
2023-09-23 17:14:41 +0300

    fix: WeatherController now secured in `controller`

--- src/main/java/com/makeitvsolo/rainytoday/controller/WeatherController.java
+++ src/main/java/com/makeitvsolo/rainytoday/controller/WeatherController.java
@@ -1,10 +1,12 @@
 package com.makeitvsolo.rainytoday.controller;
 
+import com.makeitvsolo.rainytoday.config.security.AccountPrincipal;
 import com.makeitvsolo.rainytoday.controller.response.ErrorMessageResponse;
 import com.makeitvsolo.rainytoday.service.WeatherService;
 import com.makeitvsolo.rainytoday.service.exception.account.AccountDoesNotExistsException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,10 +22,10 @@ public class WeatherController {
     }
 
     @GetMapping
-    public ResponseEntity<?> get() {
+    public ResponseEntity<?> get(@AuthenticationPrincipal AccountPrincipal principal) {
         try {
             return ResponseEntity.status(HttpStatus.OK)
-                           .body(service.getWeatherOnFavouriteLocations());
+                           .body(service.getWeatherOnFavouriteLocations(principal.getAccountId()));
         } catch (AccountDoesNotExistsException ex) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
                            .body(new ErrorMessageResponse(ex.getMessage()));
