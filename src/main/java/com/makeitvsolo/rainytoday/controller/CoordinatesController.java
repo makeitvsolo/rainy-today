4d78c1389260a25ef629683c12fcf3ad00bea8f3
makeitvsolo <makeitvsolo@gmail.com>
2023-09-23 13:56:43 +0300

    feat: impl CoordinatesController in `controller`

---
+++ src/main/java/com/makeitvsolo/rainytoday/controller/CoordinatesController.java
@@ -0,0 +1,31 @@
+package com.makeitvsolo.rainytoday.controller;
+
+import com.makeitvsolo.rainytoday.service.CoordinatesService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/coordinates")
+public class CoordinatesController {
+
+    private final CoordinatesService service;
+
+    public CoordinatesController(CoordinatesService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/location")
+    public ResponseEntity<?> searchLocation(@RequestParam("name") String locationName) {
+        try {
+            return ResponseEntity.status(HttpStatus.OK)
+                           .body(service.searchCoordinatesByName(locationName));
+        } catch (Exception ex) {
+            return ResponseEntity.internalServerError()
+                           .build();
+        }
+    }
+}
