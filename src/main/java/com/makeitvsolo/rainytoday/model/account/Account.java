30118662db1a080ea4caeb6bf90a42989031210d
makeitvsolo <makeitvsolo@gmail.com>
2023-09-23 10:53:16 +0300

    fix: Account now can give all favourites in `model`

--- src/main/java/com/makeitvsolo/rainytoday/model/account/Account.java
+++ src/main/java/com/makeitvsolo/rainytoday/model/account/Account.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 
 import java.math.BigDecimal;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @AllArgsConstructor
@@ -39,6 +40,11 @@ public final class Account {
     public boolean isInFavourites(BigDecimal latitude, BigDecimal longitude) {
         return favourites.stream()
                        .anyMatch(location -> location.isSameAs(latitude, longitude));
+    }
+
+    public List<FavouriteLocation> getAllFavourites() {
+        return favourites.stream()
+                       .toList();
     }
 
     @Override
