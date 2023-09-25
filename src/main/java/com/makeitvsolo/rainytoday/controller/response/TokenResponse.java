7c48cf7fe01f37690629812e090e72a7e0e84995
makeitvsolo <makeitvsolo@gmail.com>
2023-09-25 18:27:05 +0300

    feat: add TokenResponse in `controller.response`

---
+++ src/main/java/com/makeitvsolo/rainytoday/controller/response/TokenResponse.java
@@ -0,0 +1,11 @@
+package com.makeitvsolo.rainytoday.controller.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public final class TokenResponse {
+
+    private final String token;
+}
